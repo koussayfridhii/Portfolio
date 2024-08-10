5cdd9057c1471e386f06c6ae3a42f492ed4269b6
koussayfridhii <koussayfridhi@hotmail.fr>
2024-08-10 18:06:33 +0100

    lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,37 @@
 import { BrowserRouter } from "react-router-dom";
+import { Suspense, lazy } from "react";
 
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+// Lazy load the components
+const About = lazy(() => import('./components/About'));
+const Contact = lazy(() => import('./components/Contact'));
+const Experience = lazy(() => import('./components/Experience'));
+const Feedbacks = lazy(() => import('./components/Feedbacks'));
+const Hero = lazy(() => import('./components/Hero'));
+const Navbar = lazy(() => import('./components/Navbar'));
+const Tech = lazy(() => import('./components/Tech'));
+const Works = lazy(() => import('./components/Works'));
+const StarsCanvas = lazy(() => import('./components/StarsCanvas'));
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div className='relative z-0 bg-primary'>
-        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
-          <Navbar />
-          <Hero />
+      <Suspense fallback={<div>Loading...</div>}>
+        <div className='relative z-0 bg-primary'>
+          <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+            <Navbar />
+            <Hero />
+          </div>
+          <About />
+          <Experience />
+          <Tech />
+          <Works />
+          <Feedbacks />
+          <div className='relative z-0'>
+            <Contact />
+            <StarsCanvas />
+          </div>
         </div>
-        <About />
-        <Experience />
-        {/* <Tech />
-        <Works />
-        <Feedbacks />
-        <div className='relative z-0'>
-          <Contact />
-          <StarsCanvas />
-        </div> */}
-      </div>
+      </Suspense>
     </BrowserRouter>
   );
 }
